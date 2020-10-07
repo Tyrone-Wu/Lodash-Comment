2b8550db6b210e196aa503fc6ea60d3f001a11e8
tyrone <1161763410@qq.com>
2020-10-07 13:57:18 +0800

    init repo

--- chunk.js
+++ chunk.js
@@ -1,16 +1,18 @@
-import slice from './slice.js'
-import toInteger from './toInteger.js'
+import slice from "./slice.js";
+import toInteger from "./toInteger.js";
 
 /**
  * Creates an array of elements split into groups the length of `size`.
  * If `array` can't be split evenly, the final chunk will be the remaining
  * elements.
+ * 将数组（array）拆分成多个 size 长度的区块，并将这些区块组成一个新数组。
+ * 如果array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
  *
  * @since 3.0.0
  * @category Array
- * @param {Array} array The array to process.
- * @param {number} [size=1] The length of each chunk
- * @returns {Array} Returns the new array of chunks.
+ * @param {Array} array The array to process. 需要处理的数组
+ * @param {number} [size=1] The length of each chunk  处理之后每个数组的长度
+ * @returns {Array} Returns the new array of chunks.  返回的数组
  * @example
  *
  * chunk(['a', 'b', 'c', 'd'], 2)
@@ -20,19 +22,24 @@ import toInteger from './toInteger.js'
  * // => [['a', 'b', 'c'], ['d']]
  */
 function chunk(array, size = 1) {
-  size = Math.max(toInteger(size), 0)
-  const length = array == null ? 0 : array.length
+  // 处理传入的size，确保是正整数
+  size = Math.max(toInteger(size), 0);
+  const length = array == null ? 0 : array.length;
   if (!length || size < 1) {
-    return []
+    return [];
   }
-  let index = 0
-  let resIndex = 0
-  const result = new Array(Math.ceil(length / size))
+  // 每一次取值的其实index
+  let index = 0;
+  // 返回结果数组的index
+  let resIndex = 0;
+  // 返回的数组长度，向上取整
+  const result = new Array(Math.ceil(length / size));
 
   while (index < length) {
-    result[resIndex++] = slice(array, index, (index += size))
+    // function slice(array, start, end) {} 截取array当中start到end之间的值组成新的数组返回
+    result[resIndex++] = slice(array, index, (index += size));
   }
-  return result
+  return result;
 }
 
-export default chunk
+export default chunk;
